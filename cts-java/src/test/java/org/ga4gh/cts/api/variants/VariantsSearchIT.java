a52b1ff73a8b85493f3d5acb21c0248420c6c2bc
Herb Jellinek <jellinek@newscenter.com>
2015-09-10 15:08:24 -0700

    Added two tests of paging through Variants using SearchVariantsRequest.

--- cts-java/src/test/java/org/ga4gh/cts/api/variants/VariantsSearchIT.java
+++ cts-java/src/test/java/org/ga4gh/cts/api/variants/VariantsSearchIT.java
@@ -12,7 +12,9 @@ import org.ga4gh.models.VariantSet;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -110,7 +112,9 @@ public class VariantsSearchIT implements CtkLogs {
 
         searchVariants.stream().forEach(v -> assertThat(v.getReferenceName()).isEqualTo(TestData.REFERENCE_NAME));
 
-        searchVariants.stream().forEach(v -> v.getCalls().stream().forEach(c -> assertThat(c).isNotNull()));
+        searchVariants.stream().forEach(v -> v.getCalls()
+                                              .stream()
+                                              .forEach(c -> assertThat(c).isNotNull()));
     }
 
     /**
@@ -147,7 +151,8 @@ public class VariantsSearchIT implements CtkLogs {
                                               .forEach(c -> assertThat(c.getGenotype()).isNotNull().isNotEmpty()));
 
         searchVariants.stream().forEach(v -> v.getCalls().stream()
-                                              .forEach(c -> assertThat(c.getGenotypeLikelihood()).isNotNull()));
+                                              .forEach(c -> assertThat(c.getGenotypeLikelihood())
+                                                      .isNotNull()));
 
         searchVariants.stream().forEach(v -> v.getCalls().stream().forEach(c -> {
             assertThat(c.getInfo()).isNotNull();
@@ -157,6 +162,123 @@ public class VariantsSearchIT implements CtkLogs {
                 assertThat(c.getInfo().get(key)).isNotNull();
             });
         }));
+    }
+
+    /**
+     * Check that we can page 1 by 1 through the variants we receive from
+     * {@link org.ga4gh.ctk.transport.protocols.Client.Variants#searchVariants(SearchVariantsRequest)}.
+     *
+     * @throws AvroRemoteException if there's a communication problem or server exception ({@link GAException})
+     */
+    @Test
+    public void checkPagingOneByOneThroughCalls() throws AvroRemoteException {
+        final long start = 50;
+        final long end = 100;
+
+        final SearchVariantSetsRequest req =
+                SearchVariantSetsRequest.newBuilder()
+                                        .setDatasetId(TestData.getDatasetId())
+                                        .build();
+        final SearchVariantSetsResponse resp = client.variants.searchVariantSets(req);
+
+        final List<VariantSet> variantSets = resp.getVariantSets();
+        assertThat(variantSets).isNotEmpty();
+        final String id = variantSets.get(0).getId();
+
+        // get all variants in the range
+        final SearchVariantsRequest vReq =
+                SearchVariantsRequest.newBuilder()
+                                     .setVariantSetId(id)
+                                     .setReferenceName(TestData.REFERENCE_NAME)
+                                     .setStart(start).setEnd(end)
+                                     .build();
+        final SearchVariantsResponse vResp = client.variants.searchVariants(vReq);
+        final List<Variant> listOfVariants = vResp.getVariants();
+
+        // we will remove Variants from this Set and assert at the end that we have zero
+        final Set<Variant> setOfVariants = new HashSet<>(listOfVariants);
+        assertThat(listOfVariants).hasSize(setOfVariants.size());
+
+        // we don't care how many, as long as it's at least a few
+        assertThat(listOfVariants.size()).isGreaterThanOrEqualTo(3);
+
+        // page through the variants using the same query parameters
+        String pageToken = null;
+        for (Variant v : listOfVariants) {
+            final SearchVariantsRequest pageReq =
+                    SearchVariantsRequest.newBuilder()
+                                         .setVariantSetId(id)
+                                         .setReferenceName(TestData.REFERENCE_NAME)
+                                         .setStart(start).setEnd(end)
+                                         .setPageSize(1)
+                                         .setPageToken(pageToken)
+                                         .build();
+            final SearchVariantsResponse pageResp = client.variants.searchVariants(pageReq);
+            final List<Variant> pageOfVariants = pageResp.getVariants();
+            pageToken = pageResp.getNextPageToken();
+
+            assertThat(pageOfVariants).hasSize(1);
+            assertThat(setOfVariants).contains(pageOfVariants.get(0));
+
+            setOfVariants.remove(pageOfVariants.get(0));
+        }
+
+        assertThat(pageToken).isNull();
+        assertThat(setOfVariants).isEmpty();
+    }
+
+
+    /**
+     * Check that we can page through the variants we receive from
+     * {@link org.ga4gh.ctk.transport.protocols.Client.Variants#searchVariants(SearchVariantsRequest)}
+     * using an increment as large as the non-paged set of results.
+     *
+     * @throws AvroRemoteException if there's a communication problem or server exception ({@link GAException})
+     */
+    @Test
+    public void checkPagingByOneChunkThroughCalls() throws AvroRemoteException {
+        final long start = 50;
+        final long end = 100;
+
+        final SearchVariantSetsRequest req =
+                SearchVariantSetsRequest.newBuilder()
+                                        .setDatasetId(TestData.getDatasetId())
+                                        .build();
+        final SearchVariantSetsResponse resp = client.variants.searchVariantSets(req);
+
+        final List<VariantSet> variantSets = resp.getVariantSets();
+        assertThat(variantSets).isNotEmpty();
+        final String id = variantSets.get(0).getId();
+
+        // get all variants in the range
+        final SearchVariantsRequest vReq =
+                SearchVariantsRequest.newBuilder()
+                                     .setVariantSetId(id)
+                                     .setReferenceName(TestData.REFERENCE_NAME)
+                                     .setStart(start).setEnd(end)
+                                     .build();
+        final SearchVariantsResponse vResp = client.variants.searchVariants(vReq);
+        final List<Variant> listOfVariants = vResp.getVariants();
+
+        // we don't care how many, as long as it's at least a few
+        assertThat(listOfVariants.size()).isGreaterThanOrEqualTo(3);
+
+        // page through the variants in one gulp
+        final SearchVariantsRequest pageReq =
+                SearchVariantsRequest.newBuilder()
+                                     .setVariantSetId(id)
+                                     .setReferenceName(TestData.REFERENCE_NAME)
+                                     .setStart(start).setEnd(end)
+                                     .setPageSize(listOfVariants.size())
+                                     .build();
+        final SearchVariantsResponse pageResp = client.variants.searchVariants(pageReq);
+        final List<Variant> pageOfVariants = pageResp.getVariants();
+        final String pageToken = pageResp.getNextPageToken();
+
+        assertThat(pageOfVariants).hasSize(listOfVariants.size());
+        assertThat(listOfVariants).containsAll(pageOfVariants);
+
+        assertThat(pageToken).isNull();
     }
 
 }
