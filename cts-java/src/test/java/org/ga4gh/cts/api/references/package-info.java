545bd6571733034b268378b35e2790ca5a5920d9
Maciek Smuga-Otto <macieksmuga@gmail.com>
2015-09-22 16:38:51 -0700

    Temporarily backed out individual integration tests.

--- cts-java/src/test/java/org/ga4gh/cts/api/references/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains references-related tests, test suite, and test category.
- */
-package org.ga4gh.cts.api.references;
--- cts-java/src/test/java/org/ga4gh/cts/api/variants/VariantsTests.java
+++
@@ -1,7 +0,0 @@
-package org.ga4gh.cts.api.variants;
-
-/**
- * Marker interface for tests related to the "variants" API.
- */
-public interface VariantsTests { /* category marker */
-}
