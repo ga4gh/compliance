1c165b4f443f22d092ed179f301958236bacd6a3
Maciek Smuga-Otto <macieksmuga@gmail.com>
2015-10-05 09:45:42 -0700

    Fixed critical inconsistencies and gaps in compliance data.
    Supplemented reference data with JSON sidecar, adjusted variant metadata.
    Fixed documentation to refer to correct compliance data filenames.
    Renamed data files to indicate dataset membership.
    Fixed NCBI taxon id typo 9602 -> 9606.

--- test-data/convert_to_binary.sh
+++ test-data/convert_to_binary.sh
@@ -8,17 +8,17 @@ set -e
 ##
 
 # zip prepare the reference(s) with bgzip
-bgzip -c hs37d5_chr17_41196312-41277500.fa > hs37d5_chr17_41196312-41277500.fa.gz
+bgzip -c ref_brca1.fa > ref_brca1.fa.gz
 
 # index with samtools
-samtools faidx hs37d5_chr17_41196312-41277500.fa.gz
+samtools faidx ref_brca1.fa.gz
 
 ##
 ### variants
 ##
 
 # zip with bgzip
-bgzip -c 1000g_offset.HG00096_HG00099_HG00101.2010502.17_41196312-41277500.vcf > 1000g_offset.HG00096_HG00099_HG00101.2010502.17_41196312-41277500.vcf.gz
+bgzip -c brca1_1kgPhase3_variants.vcf > brca1_1kgPhase3_variants.vcf.gz
 
 # index with tabix
 tabix -p vcf 1000g_offset.HG00096_HG00099_HG00101.2010502.17_41196312-41277500.vcf.gz
@@ -28,9 +28,9 @@ tabix -p vcf 1000g_offset.HG00096_HG00099_HG00101.2010502.17_41196312-41277500.v
 ##
 
 # make bam files from sam files
-samtools view -b -h -o BRCA1_HG00096.bam BRCA1_HG00096_hg37_offset.sam
-samtools view -b -h -o BRCA1_HG00099.bam BRCA1_HG00099_hg37_offset.sam
-samtools view -b -h -o BRCA1_HG00101.bam BRCA1_HG00101_hg37_offset.sam
+samtools view -b -h -o brca1_HG00096.bam brca1_HG00096.sam
+samtools view -b -h -o brca1_HG00099.bam brca1_HG00099.sam
+samtools view -b -h -o brca1_HG00101.bam brca1_HG00101.sam
 
 # index bam files with samtools
 for b in *.bam; do
