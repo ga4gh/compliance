eb0ffc5031076e36f3bc63b60a4c54efab0827b2
Sarah Hunt <seh@ebi.ac.uk>
2015-12-11 16:50:35 +0000

    pull in updates from master

--- cts-java/src/test/java/org/ga4gh/cts/api/references/ReferenceSetsSearchIT.java
+++ cts-java/src/test/java/org/ga4gh/cts/api/references/ReferenceSetsSearchIT.java
@@ -58,7 +58,7 @@ public class ReferenceSetsSearchIT {
 
         final List<ReferenceSet> sets = resp.getReferenceSets();
 
-        sets.stream().forEach(this::checkRefSetConstants);
+        sets.stream().forEach(this::checkRefSetRequiredFields);
     }
 
     /**
@@ -70,7 +70,7 @@ public class ReferenceSetsSearchIT {
     public void checkReferenceSetFoundByAccession() throws AvroRemoteException {
         final SearchReferenceSetsRequest req =
                 SearchReferenceSetsRequest.newBuilder()
-                        .setAccessions(TestData.REFERENCESET_ACCESSIONS)
+                        .setAccession(TestData.REFERENCESET_ACCESSION)
                         .build();
         final SearchReferenceSetsResponse resp = client.references.searchReferenceSets(req);
 
@@ -90,7 +90,7 @@ public class ReferenceSetsSearchIT {
     public void checkReferenceSetFoundByMD5Checksum() throws AvroRemoteException {
         final SearchReferenceSetsRequest req =
                 SearchReferenceSetsRequest.newBuilder()
-                        .setMd5checksums(aSingle(TestData.REFERENCESET_MD5_CHECKSUM))
+                        .setMd5checksum(TestData.REFERENCESET_MD5_CHECKSUM)
                         .build();
         final SearchReferenceSetsResponse resp = client.references.searchReferenceSets(req);
 
@@ -128,9 +128,19 @@ public class ReferenceSetsSearchIT {
     private void checkRefSetConstants(ReferenceSet refSet) {
         assertThat(refSet.getAssemblyId()).isEqualTo(TestData.REFERENCESET_ASSEMBLY_ID);
         assertThat(refSet.getMd5checksum()).isEqualTo(TestData.REFERENCESET_MD5_CHECKSUM);
-        assertThat(refSet.getSourceAccessions()).isEqualTo(TestData.REFERENCESET_ACCESSIONS);
+        assertThat(refSet.getSourceAccessions().get(0)).isEqualTo(TestData.REFERENCESET_ACCESSION);
     }
 
+    /**
+     * Check the assembly ID, MD5 value, and accessions of a {@link ReferenceSet} is not null.
+     * @param refSet the {@link ReferenceSet}  to check
+     */
+    private void checkRefSetRequiredFields(ReferenceSet refSet) {
+        assertThat(refSet.getId()).isNotNull();
+        assertThat(refSet.getMd5checksum()).isNotNull();
+        assertThat(refSet.getSourceAccessions()).isNotEmpty();
+        assertThat(refSet.getIsDerived()).isNotNull();
+    }
     /**
      * Verify that all found references identify the right species (NCBI Taxonomy ID).
      *
