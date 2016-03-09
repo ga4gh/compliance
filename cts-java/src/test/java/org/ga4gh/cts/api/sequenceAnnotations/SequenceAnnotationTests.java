864f2c6ec07bf31347741e9e3dc09b415c75afbe
Maciek Smuga-Otto <macieksmuga@gmail.com>
2016-03-09 14:48:30 -0800

    initial commit

---
+++ cts-java/src/test/java/org/ga4gh/cts/api/sequenceAnnotations/SequenceAnnotationTests.java
@@ -0,0 +1,8 @@
+package org.ga4gh.cts.api.sequenceAnnotations;
+
+/**
+ * Marker interface for tests related to the "sequence Annotations" API.
+ */
+public interface SequenceAnnotationTests { /* category marker */
+}
+
