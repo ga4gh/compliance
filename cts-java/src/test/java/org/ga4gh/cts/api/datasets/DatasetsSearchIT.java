6afdd4eb0e8f5533fe49b9f3c164a6a7c2a858e2
David Steinberg <david@resium.com>
2016-09-02 16:59:32 -0700

    Check that version of dataset is set in info Close #215

--- cts-java/src/test/java/org/ga4gh/cts/api/datasets/DatasetsSearchIT.java
+++ cts-java/src/test/java/org/ga4gh/cts/api/datasets/DatasetsSearchIT.java
@@ -43,10 +43,17 @@ public class DatasetsSearchIT {
 
         assertThat(datasets).isNotNull();
         assertThat(datasets.size()).isGreaterThanOrEqualTo(1);
-
+        final Dataset dataset = datasets
+                .stream()
+                .filter(ds -> ds.getId().equals(TestData.getDatasetId()))
+                .findFirst().get();
+        assertThat(dataset.getName()).isNotNull();
+        assertThat(dataset.getInfo().get("version")).isNotNull();
+        assertThat(dataset.getDescription()).isNotNull();
         // check that there is exactly one dataset with the ID of the test data
         assertThat(datasets.stream().filter(ds -> ds.getId().equals(TestData.getDatasetId())).count())
                 .isEqualTo(1);
+
     }
 
     /**
